542e32494c61d8733bb83e556239146ce112e3ad
Clement Vellieux <clement.vellieux@gmail.com>
2016-08-05 10:33:58 +0430

    add resources for products and CRUD actions

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,8 @@ Rails.application.routes.draw do
   root to: 'pages#home'
   get "/team" => "pages#team"
   get "contact"=> "pages#join_us"
-  get "/products" => "products#index"
-  get "products/:id" => "products#show"
+  resources :products
+  # get "/products" => "products#index"
+  # get "products/:id" => "products#show"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
